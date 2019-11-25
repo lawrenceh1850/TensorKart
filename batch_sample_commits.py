0cb6914897388ff055564e70b1e943f47c6667ac
Lawr <lawrence1999@gmail.com>
2019-11-24 21:46:47 -0500

    util files

---
+++ batch_sample_commits.py
@@ -0,0 +1,10 @@
+import glob
+import os
+import subprocess
+
+gs_msg = subprocess.check_output("git status", shell=True)
+for folder in glob.glob("samples/*"):
+    if folder in str(gs_msg):
+        os.system(f"git add {folder}")
+        os.system(
+            f"git commit -am \"Batch commit of sample data {folder}\"")
